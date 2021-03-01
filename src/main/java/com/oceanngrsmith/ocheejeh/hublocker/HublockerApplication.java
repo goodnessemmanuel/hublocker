36cbb8ef646155fc5f42a0b1a1613ef816adfcd1
goodnessemmanuel <goodnessemmanuel@gmail.com>
2021-03-01 06:19:09 +0100

    add spring initializer boiler plate

---
+++ src/main/java/com/oceanngrsmith/ocheejeh/hublocker/HublockerApplication.java
@@ -0,0 +1,13 @@
+package com.oceanngrsmith.ocheejeh.hublocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HublockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HublockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oceanngrsmith/ocheejeh/hublocker/HublockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceanngrsmith.ocheejeh.hublocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HublockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
