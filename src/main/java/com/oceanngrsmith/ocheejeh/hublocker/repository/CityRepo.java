ad9d21dc0705d11ebcc47290ef7636fb85fedf06
apple <apple@apples-MacBook-Pro.local>
2021-06-01 22:18:03 +0100

    add repository annotation

--- src/main/java/com/oceanngrsmith/ocheejeh/hublocker/repository/CityRepo.java
+++ src/main/java/com/oceanngrsmith/ocheejeh/hublocker/repository/CityRepo.java
@@ -2,7 +2,9 @@ package com.oceanngrsmith.ocheejeh.hublocker.repository;
 
 import com.oceanngrsmith.ocheejeh.hublocker.models.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CityRepo extends JpaRepository<City, Long> {
     City findByName(String cityName);
 }
--- src/main/java/com/oceanngrsmith/ocheejeh/hublocker/repository/StateRepo.java
+++ src/main/java/com/oceanngrsmith/ocheejeh/hublocker/repository/StateRepo.java
@@ -2,7 +2,9 @@ package com.oceanngrsmith.ocheejeh.hublocker.repository;
 
 import com.oceanngrsmith.ocheejeh.hublocker.models.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StateRepo extends JpaRepository<State, Long> {
     State findByName(String name);
 }
